88c6b19eb0ada47e2fcae43daf3eb31b400e5a94
Nguyễn Tiến Dũng <dungnb27@gmail.com>
2021-03-25 14:58:49 +0700

    sitebar

--- include/slider.php
+++ include/slider.php
@@ -10,11 +10,11 @@
                                     <div class="single-slide align-center-left  animation-style-01 bg-1">
                                         <div class="slider-progress"></div>
                                         <div class="slider-content">
-                                            <h5>Sale Offer <span>-20% Off</span> This Week</h5>
-                                            <h2>Chamcham Galaxy S9 | S9+</h2>
-                                            <h3>Starting at <span>$1209.00</span></h3>
+                                            <h5>Giảm giá <span>-20% </span> </h5>
+                                            <h2>SamSung Galaxy S9 | S9+</h2>
+                                            <h3>Giá từ <span>12.000.000 VNĐ</span></h3>
                                             <div class="default-btn slide-btn">
-                                                <a class="links" href="shop-left-sidebar.html">Shopping Now</a>
+                                                <a class="links" href="#">Mua ngay</a>
                                             </div>
                                         </div>
                                     </div>
@@ -23,11 +23,11 @@
                                     <div class="single-slide align-center-left animation-style-02 bg-2">
                                         <div class="slider-progress"></div>
                                         <div class="slider-content">
-                                            <h5>Sale Offer <span>Black Friday</span> This Week</h5>
+                                            <h5>Giảm giá <span>Black Friday</span> </h5>
                                             <h2>Work Desk Surface Studio 2018</h2>
-                                            <h3>Starting at <span>$824.00</span></h3>
+                                            <h3>Giá từ <span>18.000.000 VNĐ</span></h3>
                                             <div class="default-btn slide-btn">
-                                                <a class="links" href="shop-left-sidebar.html">Shopping Now</a>
+                                                <a class="links" href="#">Mua ngay</a>
                                             </div>
                                         </div>
                                     </div>
@@ -36,11 +36,11 @@
                                     <div class="single-slide align-center-left animation-style-01 bg-3">
                                         <div class="slider-progress"></div>
                                         <div class="slider-content">
-                                            <h5>Sale Offer <span>-10% Off</span> This Week</h5>
+                                            <h5>Giảm giá <span>-10% Off</span> </h5>
                                             <h2>Phantom 4 Pro+ Obsidian</h2>
-                                            <h3>Starting at <span>$1849.00</span></h3>
+                                            <h3>Giá từ <span>12.000.000 VNĐ</span></h3>
                                             <div class="default-btn slide-btn">
-                                                <a class="links" href="shop-left-sidebar.html">Shopping Now</a>
+                                                <a class="links" href="#">Mua ngay</a>
                                             </div>
                                         </div>
                                     </div>
--- include/topbar.php
+++ include/topbar.php
@@ -5,6 +5,11 @@
 			unset($_SESSION['dangnhap_home']);  
 		}	
 	}
+
+    if(isset($_POST['giohang'])){
+		header('Location: index.php?quanly=giohang');
+        echo'giohang';
+	}
 ?>
 <!-- Begin Header Top Area -->
 <div class="header-top">
@@ -83,59 +88,20 @@
                 <div class="header-middle-right">
                     <ul class="hm-menu">
                         <!-- Begin Header Middle Wishlist Area -->
-                        <li class="hm-wishlist">
-                            <a href="wishlist.html">
-                                <span class="cart-item-count wishlist-item-count">0</span>
-                                <i class="fa fa-heart-o"></i>
-                            </a>
-                        </li>
+                        
                         <!-- Header Middle Wishlist Area End Here -->
                         <!-- Begin Header Mini Cart Area -->
                         <li class="hm-minicart">
-                            <div class="hm-minicart-trigger">
-                                <span class="item-icon"></span>
-                                <span class="item-text">£80.00
-                                    <span class="cart-item-count">2</span>
-                                </span>
-                            </div>
-                            <span></span>
-                            <div class="minicart">
-                                <ul class="minicart-product-list">
-                                    <li>
-                                        <a href="single-product.html" class="minicart-product-image">
-                                            <img src="images/product/small-size/5.jpg" alt="cart products">
-                                        </a>
-                                        <div class="minicart-product-details">
-                                            <h6><a href="single-product.html">Aenean eu tristique</a></h6>
-                                            <span>£40 x 1</span>
-                                        </div>
-                                        <button class="close" title="Remove">
-                                            <i class="fa fa-close"></i>
-                                        </button>
-                                    </li>
-                                    <li>
-                                        <a href="single-product.html" class="minicart-product-image">
-                                            <img src="images/product/small-size/6.jpg" alt="cart products">
-                                        </a>
-                                        <div class="minicart-product-details">
-                                            <h6><a href="single-product.html">Aenean eu tristique</a></h6>
-                                            <span>£40 x 1</span>
-                                        </div>
-                                        <button class="close" title="Remove">
-                                            <i class="fa fa-close"></i>
-                                        </button>
-                                    </li>
-                                </ul>
-                                <p class="minicart-total">SUBTOTAL: <span>£80.00</span></p>
-                                <div class="minicart-button">
-                                    <a href="shopping-cart.html" class="li-button li-button-fullwidth li-button-dark">
-                                        <span>View Full Cart</span>
-                                    </a>
-                                    <a href="checkout.html" class="li-button li-button-fullwidth">
-                                        <span>Checkout</span>
-                                    </a>
-                                </div>
-                            </div>
+                            
+                                <form action="" method="post">
+                                    
+                                        <input type="submit" class="item-icon" value="Giỏ hàng" name="giohang" style="background: #e80f0f;color: black;">
+                                    
+                                </form>
+                        
+
+                            
+                            
                         </li>
                         <!-- Header Mini Cart Area End Here -->
                     </ul>
